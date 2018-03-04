0571d7a38bb1c76677cb6f0a1f9f72b9f710eb22
Minh Nguyen <nguyenlenhutminh@icloud.com>
2018-03-04 15:18:45 +0700

    Initial Commit

---
+++ UseGitExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UseGitExample
+//
+//  Created by Nhut Minh Nguyen Le on 3/4/18.
+//  Copyright © 2018 minhnguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
